b50963763281d34cd7bd98c01dd45a1b74a59aa3
Lars Meijdam <lmeijdam90@gmail.com>
2018-09-12 17:00:53 +0200

    initial layout page changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dash-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
